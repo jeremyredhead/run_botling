b0aa2bbf3749f96e9a7774d35d73ab95e5f0ad89
JRF <>
2022-04-09 01:35:24 -0500

    Add minimal BotBot-less botling example

---
+++ minimal-botbotless-botling.py
@@ -0,0 +1,5 @@
+from botbot.botcollection import BotCollection
+
+botgroup = BotCollection(botbot=None)
+
+botgroup.create(nickname='MyBot', room_name='xkcd', password=None, creator='MyName', code='source code, NOT including the !createbot @BotName part')
