e397b2f96f2510b10909b148eb55cc25412d2e61
JRF <>
2022-08-29 14:08:52 -0500

    Fix & improve code somewhat
    Mostly self-explanatory.
    
    I had these changes sitting unsaved in Notepad++ since the date listed

--- run_botling.py
+++ run_botling.py
@@ -1,21 +1,31 @@
 import sys
 import re
 
-from botbot.botcollection import BotCollection
+# https://stackoverflow.com/questions/10043485/python-import-every-module-from-a-folder
+# https://stackoverflow.com/questions/1057431/how-to-load-all-modules-in-a-folder
+try:
+	from .botbot.botbot.botcollection import BotCollection
+except ImportError:
+	from .botbot.botcollection import BotCollection
+except ImportError:
+	from botbot.botcollection import BotCollection
 
 # Defaults -- set these as appropriate
-_room = 'test'
-_name = 'TestBot'
-creator = 'Ana Ni Muss'
+defaults = {
+	'room': 'test',
+	'name': 'TestBot',
+	'creator': 'Ana Ni Muss',
+}
 
 def run_file(file):
 	botgroup = BotCollection(botbot=None)
 	with open(file) as f:
 		regex = r'(?:\s*!createbot\s*(?:&(?P<room>\S+)\s+)?@(?P<name>\S+))?(?P<code>.*)'
 		m = re.match(regex, f.read(), re.DOTALL)
-	room = m.group('room') or _room
-	name = m.group('name')[:36] or _name
+	room = m.group('room') or defaults.room
+	name = (m.group('name') or defaults.name)[:36]
 	code = m.group('code')
+	creator = defaults.creator
 	botgroup.create(nickname=name, room_name=room, password=None, creator=creator, code=code)
 
 def main():
